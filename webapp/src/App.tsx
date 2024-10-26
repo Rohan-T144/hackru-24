89751f926b679d0a149d9046bb29a98649bbb2e1
Bhavyansh-Sabharwal <bhavyanshsabharwal@gmail.com>
2024-10-26 15:00:12 -0400

    Create scoring metric and model

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,31 +1,58 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import { Theme } from "@radix-ui/themes";
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-
 import ProjectsPage from './ProjectsPage';
 import RecordPage from './RecordPage';
 import ProjectDetailPage from './ProjectDetailPage';
+import Popup from './components/Popup';
 
 function App() {
+  const [isPopupVisible, setPopupVisible] = useState(false);
+  const [score, setScore] = useState(0);
+  const [content, setContent] = useState('');
+const [buttonVisible, setButtonVisible] = useState(true); // State for button visibility
 
-  return (
-    <Theme>
-      <Router>
-        <Navbar />
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<RecordPage />} />
-            <Route path="/projects/" element={<ProjectsPage />} />
-            <Route path="/projects/:id" element={<ProjectDetailPage />} />
+const receiveInput = () => {
+  const newScore = 95; 
+  const newContent = 'This is some content related to your score.'; 
 
-          </Routes>
-        </div>
-      </Router>
-    </Theme>
-  )
+  setScore(newScore);
+  setContent(newContent);
+  setPopupVisible(true);
+  setButtonVisible(false); // Hide button after clicking
+};
+
+const closePopup = () => {
+  setPopupVisible(false);
+  setButtonVisible(true); // Show button again when popup closes
+};
+
+return (
+  <Theme>
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<RecordPage />} />
+          <Route path="/projects/" element={<ProjectsPage />} />
+          <Route path="/projects/:id" element={<ProjectDetailPage />} />
+        </Routes>
+      </div>
+      {buttonVisible && ( // Show button only if buttonVisible is true
+        <button onClick={receiveInput}>Show Score</button>
+      )}
+      {isPopupVisible && (
+        <Popup 
+          score={score} 
+          content={content} 
+          onClose={closePopup} 
+        />
+      )}
+    </Router>
+  </Theme>
+);
 }
 
-export default App
+export default App;
