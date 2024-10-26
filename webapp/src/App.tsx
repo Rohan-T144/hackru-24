7a9fe0e8c2902030fb3c21ba7a7543d5d304e6b5
Arav Dhoot <aravdhoot@gmail.com>
2024-10-26 15:20:22 -0400

    minor front-end fix

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -6,28 +6,8 @@ import Navbar from './components/Navbar';
 import ProjectsPage from './ProjectsPage';
 import RecordPage from './RecordPage';
 import ProjectDetailPage from './ProjectDetailPage';
-import Popup from './components/Popup';
 
 function App() {
-  const [isPopupVisible, setPopupVisible] = useState(false);
-  const [score, setScore] = useState(0);
-  const [content, setContent] = useState('');
-const [buttonVisible, setButtonVisible] = useState(true); // State for button visibility
-
-const receiveInput = () => {
-  const newScore = 95; 
-  const newContent = 'This is some content related to your score.'; 
-
-  setScore(newScore);
-  setContent(newContent);
-  setPopupVisible(true);
-  setButtonVisible(false); // Hide button after clicking
-};
-
-const closePopup = () => {
-  setPopupVisible(false);
-  setButtonVisible(true); // Show button again when popup closes
-};
 
 return (
   <Theme>
@@ -40,16 +20,6 @@ return (
           <Route path="/projects/:id" element={<ProjectDetailPage />} />
         </Routes>
       </div>
-      {buttonVisible && ( // Show button only if buttonVisible is true
-        <button onClick={receiveInput}>Show Score</button>
-      )}
-      {isPopupVisible && (
-        <Popup 
-          score={score} 
-          content={content} 
-          onClose={closePopup} 
-        />
-      )}
     </Router>
   </Theme>
 );
